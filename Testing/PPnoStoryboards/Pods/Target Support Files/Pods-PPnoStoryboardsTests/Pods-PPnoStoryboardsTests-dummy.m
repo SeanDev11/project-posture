e7ec984a1c6ca59129756d8eea3005f52767e1e4
SeanDev11 <sean.devine24@googlemail.com>
2022-10-13 15:37:10 -0700

    Renamed Testing folder

--- Testing/PPnoStoryboards/Pods/Target Support Files/Pods-PPnoStoryboardsTests/Pods-PPnoStoryboardsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PPnoStoryboardsTests : NSObject
-@end
-@implementation PodsDummy_Pods_PPnoStoryboardsTests
-@end
--- Testing/testingTableView/testingTableView/cellCustomTest.swift
+++
@@ -1,9 +0,0 @@
-//
-//  cellCustomTest.swift
-//  testingTableView
-//
-//  Created by Sean Devine on 02.08.19.
-//  Copyright © 2019 Sean Devine. All rights reserved.
-//
-
-import Foundation
